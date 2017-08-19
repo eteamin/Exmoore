332967828128518dbc0880cee8d00ff280738af7
Amin Etesamian <aminetesamian1371@gmail.com>
2017-08-19 16:46:57 -0700

    DownloadTask model for single download

--- main.py
+++ main.py
@@ -1,14 +1,14 @@
 from functools import partial
-from os import path
-from tempfile import mkdtemp
 
 from kivy.app import App
 from kivy.uix.textinput import TextInput
 from kivy.uix.button import Button
+from kivy.uix.progressbar import ProgressBar
 from kivy.uix.screenmanager import ScreenManager, Screen, FadeTransition
 from kivy.network.urlrequest import UrlRequest
 
 screen_manager = ScreenManager(transition=FadeTransition())
+progress_bar = ProgressBar()
 
 
 class MainScreen(Screen):
@@ -17,31 +17,16 @@ class MainScreen(Screen):
 
         self.url_input = TextInput(pos_hint={'center_y': 0.5}, size_hint=(None, None), size=(1000, 50))
         self.start = Button(text='Start', size_hint=(None, None), size=(100, 50))
-        """binding start button release"""
+
         self.start.bind(on_release=partial(self.on_start_press))
 
         self.add_widget(self.url_input)
         self.add_widget(self.start)
+        UrlRequest('http://pushyab.com/css/style.css', on_success=self.on_start_press)
 
     def on_start_press(self, *args):
         """Get the headers to know the content-length"""
-        UrlRequest(url=self.url_input.text, on_success=self.on_head_req_success, method='HEAD')
-
-    def on_head_req_success(self, req, resp):
-        """Headers are here!"""
-        if resp:
-            raise
-        content_length = int(req.resp_headers.get('Content-Length'))
-        chunks = content_length / 2
-
-    def generate_tmp_storage(self):
-        return path.join(mkdtemp(), '1.tmp')
-
-
-
-
-
-
+        pass
 
 
 class Exmoore(App):
---
+++ models/task.py
@@ -0,0 +1,80 @@
+from uuid import uuid1
+from tempfile import mkdtemp
+from os import path
+from queue import Queue
+from threading import Thread
+
+from kivy.network.urlrequest import UrlRequest
+
+
+class DownloadTask(object):
+    def __init__(self, url: str, threads: int, download_report: Queue):
+        self.url = url
+        self.threads = threads
+        self.content_length = 0
+        self.download_report = download_report
+        self.bytes_downloaded_per_thread = {}
+        self.requests = {}
+        self._get_headers()
+
+    def _get_headers(self):
+        UrlRequest(url=self.url, on_success=self._on_headers_fetched, method='HEAD')
+
+    def _on_headers_fetched(self, req, resp):
+        self.content_length = req.resp_headers.get('Content-Length')
+        self._start_requests()
+
+    def _start_requests(self):
+        """
+        
+            Each requests takes responsibility for a part of the file by passing range to its header.
+            Storing the requests to self.requests; key: thread.ident, value: temp_path
+            
+        """
+        shares = self.content_length / self.threads
+        for i in range(self.threads):
+            bytes_range = 'bytes={}-{}'.format(i * shares, (shares + (i * shares)))
+            temp_path = self.temp_path()
+            thread = UrlRequest(
+                self.url,
+                file_path=temp_path,
+                req_headers={'Range': bytes_range},
+                on_progress=self._on_requests_progress,
+                on_success=self._on_request_success
+            )
+            self.requests[thread] = temp_path
+
+    def _on_requests_progress(self, req, current, total):
+        self.bytes_downloaded_per_thread[req.ident] = current / total
+        self._calculate_total_downloaded()
+
+    def _on_request_success(self, req, resp):
+        """Append the downloaded files"""
+        # Check to see if all threads are done
+        for thread, tmp_path in self.requests.items():
+            if thread.is_alive():
+                return
+        finalize = Thread(target=self._wrap_them_up, daemon=True)
+        finalize.start()
+
+    def _wrap_them_up(self):
+        # Read files one by one and put them all together
+        with open('final', 'wb') as final:
+            for thread, tmp_path in self.requests.items():
+                with open(tmp_path, 'rb') as tmp:
+                    final.write(tmp.read())
+
+    def _calculate_total_downloaded(self):
+        total = 0
+        for k, v in self.bytes_downloaded_per_thread.items():
+            total += v
+        self.download_report.put(total)
+
+    @staticmethod
+    def temp_path():
+        """Suggesting a temporary file for storing the downloaded chunk"""
+        return path.join(mkdtemp(), uuid1())
+
+
+
+
